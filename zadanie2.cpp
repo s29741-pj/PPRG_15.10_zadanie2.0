05c7ad8b41dae5ca6018f8f9a4224a6ba690094e
s29741-pj <s29741@pjwstk.edu.pl>
2023-10-14 11:14:22 +0200

    initial

---
+++ zadanie2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <list>
+#include <typeinfo>
+using namespace std;
+
+int main() {
+    int numOfPizzaSlices;
+    int restOfPizzaSlices;
+    int guests;
+    int hostSlices;
+
+cout << "Podaj ilość kawałków pizzy:";
+cin >> numOfPizzaSlices;
+
+cout << "Podaj liczbę gości:";
+cin >> guests;
+
+restOfPizzaSlices =  numOfPizzaSlices%guests;
+
+hostSlices -= restOfPizzaSlices;
+
+cout << "Dla gospodarza pozostało " << restOfPizzaSlices << " kawałków pizzy.";
+
+}
+
